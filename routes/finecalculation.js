9673a06ee5f0b7fca153bc371056b111c67a3245
DESKTOP-8I8EIT6\Azhar ayat <Azharhayat271@gmail.com>
2023-12-31 01:32:13 +0500

    issues fix

--- routes/finecalculation.js
+++ routes/finecalculation.js
@@ -1,9 +1,9 @@
 // fine.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Assuming you have a Fine schema defined in a separate file
-const Fine = require('./../schema/finecalculation');
+const Fine = require("./../schema/finecalculation");
 
 router.use(express.json());
 
@@ -11,42 +11,49 @@ router.use(express.json());
 const defaultFinePerDay = 10;
 
 // Get fine per day
-router.get('/get', async (req, res) => {
+// Get fine data including ID
+router.get("/get", async (req, res) => {
   try {
     const fineData = await Fine.findOne(); // Assuming you have only one record for fine data
 
     if (!fineData) {
       // If no fine data exists, return the default value
-      return res.status(200).json({ finePerDay: defaultFinePerDay });
+      return res.status(200).json({ id: null, finePerDay: defaultFinePerDay });
     }
 
-    res.status(200).json({ finePerDay: fineData.finePerDay });
+    res.status(200).json({ id: fineData._id, finePerDay: fineData.finePerDay });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ error: "Server error" });
   }
 });
 
 // Update fine per day
-router.put('/update', async (req, res) => {
-  const { finePerDay } = req.body;
+// Update fine per day based on ID in the request body
+router.put("/update", async (req, res) => {
+  const { id, finePerDay } = req.body;
 
   try {
-    let fineData = await Fine.findOne(); // Assuming you have only one record for fine data
-
-    if (!fineData) {
-      // If no fine data exists, create a new record
-      fineData = new Fine({ finePerDay: finePerDay || defaultFinePerDay });
-    } else {
-      // Update existing record
-      fineData.finePerDay = finePerDay || fineData.finePerDay;
+    if (!id) {
+      res.status(400).json({ error: "ID is required in the request body" });
+      return;
     }
 
+    let fineData = await Fine.findById(id);
+
+    if (!fineData) {
+      res.status(404).json({ error: "Fine data not found" });
+      return;
+    }
+
+    // Update existing record
+    fineData.finePerDay = finePerDay || fineData.finePerDay;
+
     await fineData.save();
-    res.status(200).json({ message: 'Fine per day updated successfully' });
+    res.status(200).json({ message: "Fine per day updated successfully" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ error: "Server error" });
   }
 });
 
